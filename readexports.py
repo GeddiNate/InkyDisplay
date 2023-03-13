05c93f19ff53798b3c351cd2142ed07549861452
Nathan <nathangeddis.98@gmail.com>
2023-03-12 22:43:57 -0700

    started using selenium

--- readexports.py
+++ readexports.py
@@ -1,9 +1,41 @@
 from bs4 import BeautifulSoup
 import quote
+import jsonpickle
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-f = open('Exports/The Way of Kings (The Stormlight Archive, Book 1)-Notebook.html', 'r', encoding="utf-8") 
 
-soup = BeautifulSoup(f, 'html.parser')
+email = ""
+password = ""
+
+def main():
+    print("Hello World!")
+    driver = webdriver.Firefox()
+    driver.get("https://read.amazon.com/")
+    assert "Kindle" in driver.title
+    driver.find_element(By.ID, "top-sign-in-btn").click() #sign in on main page
 
 
-print(soup.prettify())
+
+    #login
+    elem = driver.find_element(By.ID, 'ap_email')
+    elem.send_keys(email)
+    
+    elem = driver.find_element(By.ID, 'ap_password')
+    elem.send_keys(password)
+
+    driver.find_element(By.NAME, 'rememberMe').click()
+
+    driver.find_element(By.ID, 'signInSubmit')    
+
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
