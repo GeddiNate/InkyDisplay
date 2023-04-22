13cc2310ed9d8a3faebf8464714f0b10ebe2e9d7
Nathan Geddis <nathangeddis.98@gmail.com>
2023-04-21 17:34:49 -0700

    m:

--- syncnotes.py
+++ syncnotes.py
@@ -6,7 +6,7 @@ import datetime
 # import undetected_chromedriver as webdriver
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from ast import literal_eval
 
