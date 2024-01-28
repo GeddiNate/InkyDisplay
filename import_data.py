8d84f53f955010c7ca17659cfdce442f96541621
Nathan Geddis <nathangeddis.98@gmail.com>
2024-01-27 22:18:45 -0800

    import from csv done

--- import_data.py
+++ import_data.py
@@ -1,7 +1,19 @@
 import sqlite3
 import csv
+import os
+import re
 
 note_sep = '=========='
+test_dir_path = 'csv'
+
+TITLE_LINE = 1
+AUTHOR_LINE = 2
+FIRST_DATA_ROW = 8
+TYPE_COLUMN = 0
+LOCATION_COLUMN = 1
+CONTENT_COLUMN = 3
+
+
 
 def from_my_clippings_(cur, filename):
     with open(filename, encoding='utf-8-sig') as file:
@@ -10,57 +22,93 @@ def from_my_clippings_(cur, filename):
         print(data)
 
 def from_emailed_csv(cur, filename):
-    with open(filename, encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         reader = list(csv.reader(file))
 
-        title, subtitle = reader[1][0].split(':')
-        title.strip().title()
-        subtitle.strip().title()
+        # Get title and subtitle from the csv file.
+        tmp = re.split(r'[:\(]', reader[TITLE_LINE][TYPE_COLUMN])
+        title = tmp[0].strip().title()
+        subtitle = tmp[1].strip().title()
 
-        author = reader[2][0][2:].strip()
+        if ')' in subtitle: # Remove closeing parenthetical from the subtitle.
+            subtitle = subtitle[:-1]
 
-        cur.execute("INSERT INTO books (title, subtitle) VALUES (?,?)",
-            (title.strip().title(), subtitle.strip().title())
-            )
-        book_id = cur.lastrowid
+        # Get the author(s) and remove the leading "by".
+        authors_text = reader[AUTHOR_LINE][TYPE_COLUMN][2:].strip()
+        if ',' in authors_text: # if multiple authors sperated by comma
+            authors = [item.strip() for item in authors_text.split(',')]
+        else: # Put the author in a single element array so there is only one itteration.
+            authors = [authors_text]  
         
-        cur.execute("INSERT INTO authors (name) VALUES (?)",
-            (author,)
-            )
-        author_id = cur.lastrowid
-
-        cur.execute("INSERT INTO book_authors (book_id, author_id) VALUES (?,?)",
-            (book_id, author_id)
-            )
-
-        for row in reader[8:]:
-            content_type = row[0].split(' ')[0]
-            location = int(row[1].split(' ')[1])
-            content = row[3]
-            if content_type == 'Highlight':
-                cur.execute("INSERT INTO highlights (content, location, book_id) VALUES (?,?,?)",
-                    (content, location, book_id)
+        author_ids = []
+        for author in authors:
+            try: # Insert author into the DB.
+                cur.execute("INSERT INTO authors (name) VALUES (?)",
+                    (author,)
                     )
-            elif content_type == 'Note':
-                fetched_row = cur.execute('SELECT highlight_id FROM highlights WHERE book_id=? AND location=?',
-                (book_id, location)
-                ).fetchall()
-                #print(fetched_row[0]['highlight_id'])
-                if fetched_row:
-                    cur.execute('UPDATE highlights SET note=? WHERE highlight_id=?',
-                    (content, fetched_row[0]['highlight_id'])
+                author_ids.append(cur.lastrowid) # Save the author_id for later.
+            except sqlite3.IntegrityError as e: # If it is already in the DB get the author_id.
+                cur.execute("SELECT author_id FROM authors WHERE name=?",
+                    (author,)
+                    )
+                author_ids.append(cur.fetchone()[0]) # Save the author_id for later.
+
+        try: # Insert book if it is not in the DB.
+            cur.execute("INSERT INTO books (title, subtitle) VALUES (?,?)",
+                (title, subtitle)
+                )
+            book_id = cur.lastrowid # Save the book_id for later.
+        except sqlite3.IntegrityError as e: # If it is in the DB get the book_id.
+            cur.execute("SELECT book_id FROM books WHERE title=? AND subtitle=?",
+                (title, subtitle)
+                )
+            book_id = cur.fetchone()[0] # Save the book_id for later.
+        else: # if a new book is inserted link it to each author.
+            for author_id in author_ids: 
+                cur.execute("INSERT INTO book_authors (book_id, author_id) VALUES (?,?)",
+                (book_id, author_id)
+                )
+
+        highlight_rows = reader[FIRST_DATA_ROW:]
+        last_highlight_id = None
+        for i in range(len(highlight_rows)): # For each highlight or note
+            row = highlight_rows[i]
+
+            content_type = row[TYPE_COLUMN]
+            loc_type, location = row[LOCATION_COLUMN].split(' ') # loc_type is the page or location. Location contains the number.
+            
+            # Clean data so it matches the field names in the DB.
+            if loc_type == "Location": 
+                loc_type = 'location_start'
+            else: # The page field in DB is lowerae
+                loc_type = loc_type.lower()
+
+            content = row[CONTENT_COLUMN] # Get the highlighted text or the note.
+            if 'Highlight' in content_type:
+                try : # Insert Highlight if it is not in the DB.
+                    content_type, color = content_type.split(' ')
+                    cur.execute(f"INSERT INTO highlights (content, color, {loc_type}, book_id) VALUES (?,?,?,?)",
+                        (content, color, location, book_id)
+                        ) 
+                    last_highlight_id = cur.lastrowid
+                except sqlite3.IntegrityError as e:
+                    print(f'Insert failed due to unique constraint: {e}')
+            elif 'Note' in content_type:
+                """ All my notes are on highlighted passages and there is only one note per passages
+                    so this works for me. In the future I should find a better solution than this but
+                    I'm going to move on to other aspects of this project for now."""
+                cur.execute(f"INSERT INTO notes (note, {loc_type}, highlight_id, book_id) VALUES (?,?,?,?)",
+                    (content, location, last_highlight_id, book_id)
                     )
 
-    
-def get_db_connection():
+if __name__ == '__main__':
+    # FOR TESTING
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row 
-    return conn
+    cur = conn.cursor()
+    for filename in os.listdir(test_dir_path):
+        filepath = os.path.join(test_dir_path, filename)
+        from_emailed_csv(cur, filepath)
 
-conn = get_db_connection() 
-cur = conn.cursor()
-#from_kindle_clippings_file('tmp', 'My Clippings.txt')
-from_emailed_csv(cur, 'Oathbringer_ Book Three of the Stormlight Archive-Notebook.csv')
-
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS highlights;
+DROP TABLE IF EXISTS notes;
 DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS authors;
 DROP TABLE IF EXISTS artwork;
@@ -9,25 +10,45 @@ DROP TABLE IF EXISTS book_art;
 CREATE TABLE highlights (
     highlight_id INTEGER PRIMARY KEY AUTOINCREMENT,
     content TEXT NOT NULL,
-    location INTEGER NOT NULL,
-    visible INTEGER NOT NULL DEFAULT TRUE,
+    color TEXT DEFAULT NUll,
+    location_start INTEGER DEFAULT NULL,
+    location_end INTEGER DEFAULT NULL,
+    page INTEGER DEFAULT NULL,
+    showable INTEGER NOT NULL DEFAULT TRUE,
+    shown INTEGER NOT NULL DEFAULT FALSE,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    note TEXT DEFAULT NULL,
-    book_id INTEGER,
+    book_id INTEGER DEFAULT NULL,
+    UNIQUE(content, book_id),
     FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
+
+-- Table to store notes from kindle
+CREATE TABLE notes (
+    note_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    note TEXT NOT NULL,
+    location_start INTEGER DEFAULT NULL,
+    location_end INTEGER DEFAULT NULL,
+    page INTEGER DEFAULT NULL,
+    highlight_id INTEGER DEFAULT NULL,
+    book_id INTEGER DEFAULT NULL,
+    FOREIGN KEY (highlight_id) REFERENCES highlights(highlight_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id),
+    UNIQUE(note, location_start, location_end, page, highlight_id, book_id)
 );
 
 -- Table to store information about books
 CREATE TABLE books (
     book_id INTEGER PRIMARY KEY AUTOINCREMENT,
     title VARCHAR(255) NOT NULL,
-    subtitle VARCHAR(255) NOT NULL
+    subtitle VARCHAR(255) DEFAULT NULL,
+    UNIQUE (title, subtitle)
 );
 
 -- Table to store information about authors
 CREATE TABLE authors (
     author_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    UNIQUE (name)
 );
 
 -- Table to store filepaths for artwork
@@ -54,7 +75,6 @@ CREATE TABLE book_art (
     PRIMARY KEY (book_id, art_id),
     FOREIGN KEY (book_id) REFERENCES books(book_id),
     FOREIGN KEY (art_id) REFERENCES artwork(art_id)
-
 );
 
 
